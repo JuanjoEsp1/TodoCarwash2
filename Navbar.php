2c1082f21b9c0e844ff2c6d7550944471c313c5c
juanjoesp1 <espinozajuanjo26@gmail.com>
2022-09-23 05:29:37 +0000

    d

---
+++ Navbar.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<nav>
+        <ul class="ul-1">
+            <li><a class="a-1" href="Index.php">Principal</a></li>
+            <li><a class="a-2" href="MostrarEmpresas.php">Empresas de Lavado</a></li>
+            <li><a class="a-3" href="Contacto.php">Contacto</a></li>
+            <li><a class="a-4" href="">Quienes Somos</a></li>
+            <li><a class="a-5" href="Registrar.php">Registrar</a></li>
+            <li><a class="a-6" href="Login.php">Iniciar Sesion</a></li>
+            <li><a class="a-6" href="#" onclick="document.getElementById('id01').style.display='block'" style="width:auto;">Iniciar Sesion2</a></li>
+        </ul>
+    </nav>
+    
+</body>
+</html>
