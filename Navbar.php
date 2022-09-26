b177553dc24144f40514f6b4a6a1028ce9b404ef
juanjoesp1 <espinozajuanjo26@gmail.com>
2022-09-26 21:58:37 +0000

    a

--- Navbar.php
+++ Navbar.php
@@ -1,23 +1,28 @@
+<?php include("Login2.php")
+?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="Css/Login2.css" type="text/css" />
+    <link rel="stylesheet" href="Css/Navbar.css" type="text/css" />
 </head>
+
 <body>
-<nav>
-        <ul class="ul-1">
-            <li><a class="a-1" href="Index.php">Principal</a></li>
-            <li><a class="a-2" href="MostrarEmpresas.php">Empresas de Lavado</a></li>
-            <li><a class="a-3" href="Contacto.php">Contacto</a></li>
-            <li><a class="a-4" href="">Quienes Somos</a></li>
-            <li><a class="a-5" href="Registrar.php">Registrar</a></li>
-            <li><a class="a-6" href="Login.php">Iniciar Sesion</a></li>
-            <li><a class="a-6" href="#" onclick="document.getElementById('id01').style.display='block'" style="width:auto;">Iniciar Sesion2</a></li>
+    <nav>
+        <ul>
+            <li><a class="active"  href="Index.php">Principal</a></li>
+            <li><a href="MostrarEmpresas.php">Empresas de Lavado</a></li>
+            <li><a href="Contacto.php">Contacto</a></li>
+            <li><a href="">Quienes Somos</a></li>
+            <li><a href="Registrar.php">Registrar</a></li>
+            <li><a href="#" onclick="document.getElementById('id01').style.display='block'" style="width:auto;">Iniciar Sesion</a></li>
         </ul>
     </nav>
-    
+
 </body>
+
 </html>
