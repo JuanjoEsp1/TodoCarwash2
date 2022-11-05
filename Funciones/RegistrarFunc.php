331bbfc07bfaa7cb1ab8bb277e6946206c9667fd
JuanjoEsp1 <you@example.com>
2022-11-05 06:26:14 +0000

    sdsd

--- Funciones/RegistrarFunc.php
+++ Funciones/RegistrarFunc.php
@@ -11,25 +11,25 @@
 
 <body>
         <?php
+
+        date_default_timezone_set("America/Santiago");
+        $fecha=date("Y-m-d");
         include('../Funciones/db.php');
 
-        mysqli_query($conexion, "insert into empresa(rut_empresa,nombre_empresa,calle,numeracion,comuna,telefono_empresa,correo_empresa,contrasena) values ('$_REQUEST[rut_empresa]','$_REQUEST[nombre_empresa]','$_REQUEST[calle]','$_REQUEST[numeracion]','$_REQUEST[comuna]','$_REQUEST[telefono_empresa]','$_REQUEST[correo_empresa]','$_REQUEST[contrasena]')")
+        mysqli_query($conexion, "insert into empresa(rut_empresa,nombre_empresa,calle,numeracion,comuna,telefono_empresa,correo_empresa,contrasena,fecha) VALUES
+         ('$_REQUEST[rut_empresa]','$_REQUEST[nombre_empresa]','$_REQUEST[calle]','$_REQUEST[numeracion]','$_REQUEST[comuna]','$_REQUEST[telefono_empresa]','$_REQUEST[correo_empresa]','$_REQUEST[contrasena]','$fecha')")
 
                 or die("Problemas en la consulta" . mysqli_error($conexion));
 
         mysqli_close($conexion);
 
 
-        
+
         header("location:/Index.php");
 
         ?>
 
 
 </body>
-
-
-
-
 
 </html>
--- Perfil.php
+++ Perfil.php
@@ -26,10 +26,10 @@ $idEmpresa = $row['idEmpresa'];
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-    
+
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
     <link rel="stylesheet" href="Css/Perfil.css" type="text/css" />
     <title>Perfil</title>
@@ -60,10 +60,13 @@ $idEmpresa = $row['idEmpresa'];
     WHERE agendamiento.EMPRESA_idEmpresa = '$idEmpresa' AND fecha > (Now() - INTERVAL 1 DAY) ORDER BY fecha");
 
     ?>
-
+    <div class="pago">
+        <button type="button" class="btnpago" onclick="location.href='https://www.flow.cl/btn.php?token=i90ype2'">Suscripcion</button>
+    </div>
     <div class="container">
 
         <h1>Bienvenido: <?php echo utf8_encode($row['nombre_empresa']); ?></h1>
+
         <hr>
         <h1>Horas Agendadas</h1>
         <hr>
