38a70efa46f95e8a6c4d4dd74ea6e3e4d2c90e3e
JuanjoEsp1 <you@example.com>
2022-11-06 19:41:13 +0000

    ca

--- DetalleEmpresa.php
+++ DetalleEmpresa.php
@@ -10,6 +10,10 @@ include("Funciones/db.php");
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="apple-touch-icon" sizes="180x180" href="Image/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="Image/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="Image/favicon-16x16.png">
+    <link rel="manifest" href="Image/site.webmanifest">
     <link rel="stylesheet" href="Css/detalleEmpresa.css" type="text/css" />
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAwPrp3BT2yJmxJQxIpIGNHn_p0hXxiTU8&sensor=false"></script>
     <title>Datos de empresa</title>
@@ -60,7 +64,8 @@ include("Funciones/db.php");
 
     <?php }
             } ?>
-
+    <h1>mapa</h1>
+    <div id="map"></div>
     <article class="detail-content">
         <div class="detail">
             <h1 class="nombre_empresa"><?php echo $row["nombre_empresa"]; ?></h1>
@@ -74,139 +79,139 @@ include("Funciones/db.php");
             </div>
         </div>
     </article>
+
         </section>
-        <h1>mapa</h1>
-    <div id="map"></div>
 
-    <?php
-    
-    // Select all the rows in the markers table
-    $query = "SELECT * FROM empresa WHERE idEmpresa = '$nik'";
-    $resultmap = mysqli_query($conexion, $query);
-    if (!$resultmap) {
-        die('Invalid query: ');
-    }
-    while ($rowmap = @mysqli_fetch_assoc($resultmap)) {
-    
-    ?>
-
-<script type="text/javascript">
-            var markers = [];
-            var icon_ = "https://cdn2.iconfinder.com/data/icons/font-awesome/1792/map-marker-32.png";
-
-            // con código PHP lo puedes agregar directamente 
-            // en él codigo los estilos y etiquetas 
-            // que vayas a necesitar
-            //puedes agregar todo el contenido que necesites
-            var locations = [
-                [
-                    '<p class="strong"><?php echo $rowmap["nombre_empresa"]; ?></p>',
-                    <?php echo $rowmap["latitude"]; ?>, //lat
-                    <?php echo $rowmap["longitude"]; ?>, //lon
-                    icon_,
-                    '<?php echo $rowmap["direccion"]; ?>',
-                ],
-
-            ];
-            
-
-            var map = new google.maps.Map(document.getElementById('map'), {
-                zoom: 14,
-                center: new google.maps.LatLng(<?php echo $rowmap["latitude"]; ?>, <?php echo $rowmap["longitude"]; ?>),
-                mapTypeId: google.maps.MapTypeId.ROADMAP
-            });
-            <?php } ?>
-
-            var infowindow = new google.maps.InfoWindow();
-
-            for (i = 0; i < locations.length; i++) {
-
-                //access to global - var #markers
-                markers[i] = new google.maps.Marker({
-                    position: new google.maps.LatLng(locations[i][1], locations[i][2]), //lat,lon
-                    map: map, //mapa
-                    icon: locations[i][3] //custom icon
-                });
-
-                google.maps.event.addListener(markers[i], 'click', (function(marker, i) {
-                    return function() {
-                        infowindow.setContent(locations[i][0] + "<br>" + locations[i][4]); //puede ser contenido HTML
-                        infowindow.open(map, marker);
-                    }
-                })(markers[i], i));
-            }
-        </script>
-        
-        
-        <!---------------------------------------------------------------->
 
         <?php
 
-        $sql2 = mysqli_query($conexion, "SELECT idSERVICIO, nombre_servicio,precio_servicio FROM servicio WHERE EMPRESA_idEmpresa='$nik'");
-
-        $sql3 = mysqli_query($conexion, "SELECT idHORAS, fecha, hora FROM horas WHERE EMPRESA_idEmpresa='$nik' AND disponible = 'si' AND fecha > (Now() - INTERVAL 1 DAY) ORDER BY fecha, hora");
+        // Select all the rows in the markers table
+        $query = "SELECT * FROM empresa WHERE idEmpresa = '$nik'";
+        $resultmap = mysqli_query($conexion, $query);
+        if (!$resultmap) {
+            die('Invalid query: ');
+        }
+        while ($rowmap = @mysqli_fetch_assoc($resultmap)) {
 
         ?>
 
-        <div id="id02" class="container">
-            <div class="title">Agendar</div>
-            <div class="content">
-                <form class="modal-content animate" action="Funciones/Agendar.php" method="post">
+            <script type="text/javascript">
+                var markers = [];
+                var icon_ = "https://cdn2.iconfinder.com/data/icons/font-awesome/1792/map-marker-32.png";
 
-                    <div class="user-details">
-                        <div class="input-box">
-                            <span class="details" for="nomCLIENTE">Nombres</span>
-                            <input type="text" placeholder="Ingrese su nombre" name="nomCLIENTE" required>
+                // con código PHP lo puedes agregar directamente 
+                // en él codigo los estilos y etiquetas 
+                // que vayas a necesitar
+                //puedes agregar todo el contenido que necesites
+                var locations = [
+                    [
+                        '<p class="strong"><?php echo $rowmap["nombre_empresa"]; ?></p>',
+                        <?php echo $rowmap["latitude"]; ?>, //lat
+                        <?php echo $rowmap["longitude"]; ?>, //lon
+                        icon_,
+                        '<?php echo $rowmap["direccion"]; ?>',
+                    ],
+
+                ];
+
+
+                var map = new google.maps.Map(document.getElementById('map'), {
+                    zoom: 14,
+                    center: new google.maps.LatLng(<?php echo $rowmap["latitude"]; ?>, <?php echo $rowmap["longitude"]; ?>),
+                    mapTypeId: google.maps.MapTypeId.ROADMAP
+                });
+                <?php } ?>
+
+                var infowindow = new google.maps.InfoWindow();
+
+                for (i = 0; i < locations.length; i++) {
+
+                    //access to global - var #markers
+                    markers[i] = new google.maps.Marker({
+                        position: new google.maps.LatLng(locations[i][1], locations[i][2]), //lat,lon
+                        map: map, //mapa
+                        icon: locations[i][3] //custom icon
+                    });
+
+                    google.maps.event.addListener(markers[i], 'click', (function(marker, i) {
+                        return function() {
+                            infowindow.setContent(locations[i][0] + "<br>" + locations[i][4]); //puede ser contenido HTML
+                            infowindow.open(map, marker);
+                        }
+                    })(markers[i], i));
+                }
+            </script>
+
+
+            <!---------------------------------------------------------------->
+
+            <?php
+
+            $sql2 = mysqli_query($conexion, "SELECT idSERVICIO, nombre_servicio,precio_servicio FROM servicio WHERE EMPRESA_idEmpresa='$nik'");
+
+            $sql3 = mysqli_query($conexion, "SELECT idHORAS, fecha, hora FROM horas WHERE EMPRESA_idEmpresa='$nik' AND disponible = 'si' AND fecha > (Now() - INTERVAL 1 DAY) ORDER BY fecha, hora");
+
+            ?>
+
+            <div id="id02" class="container">
+                <div class="title">Agendar</div>
+                <div class="content">
+                    <form class="modal-content animate" action="Funciones/Agendar.php" method="post">
+
+                        <div class="user-details">
+                            <div class="input-box">
+                                <span class="details" for="nomCLIENTE">Nombres</span>
+                                <input type="text" placeholder="Ingrese su nombre" name="nomCLIENTE" required>
+                            </div>
+                            <div class="input-box">
+                                <span class="details" for="apellCLIENTE">Apellidos</span>
+                                <input type="text" placeholder="Ingrese su apellido" name="apellCLIENTE" required>
+                            </div>
+                            <div class="input-box">
+                                <span class="details" for="rutCLIENTE">Rut</span>
+                                <input type="text" placeholder="Ingrese su Rut" name="rutCLIENTE" oninput="checkRut(this)" required>
+                            </div>
+                            <div class="input-box">
+                                <span class="details" for="dirCLIENTE">Direccion</span>
+                                <input type="text" placeholder="Ingrese su Direccion" name="dirCLIENTE" required>
+                            </div>
+                            <div class="input-box">
+                                <span class="details" for="numCLIENTE">Numero Celular</span>
+                                <input class="code" type="text" placeholder="+56" readonly>
+                                <input type="tel" class="tel" name="numCLIENTE" placeholder="12345678" maxlength="8" required>
+                            </div>
+                            <div class="input-box">
+                                <span class="details" for="emailCLIENTE">Correo electronico</span>
+                                <input type="email" name="emailCLIENTE" placeholder="correo@gmail.com" required>
+                            </div>
+                            <div class="select-box">
+                                <span class="details">servicio:</span>
+                                <select name="cbx_servicios" id="cbx_servicios">
+                                    <option value="0">seleccionar un servicio</option>
+                                    <?php while ($row = $sql2->fetch_assoc()) {
+                                    ?>
+                                        <option value="<?php echo $row['idSERVICIO']; ?>"><?php echo $row['nombre_servicio']; ?><?php echo ' - ' ?><?php echo '$' ?><?php echo $row['precio_servicio']; ?></option>
+                                    <?php } ?>
+                                </select>
+                            </div>
+                            <div class="select-box">
+                                <span class="details">Seleccion Hora:</span>
+                                <select name="cbx_horas" id="cbx_horas">
+                                    <option value="0">seleccionar Hora</option>
+                                    <?php while ($row = $sql3->fetch_assoc()) {
+                                    ?>
+                                        <option value="<?php echo $row['idHORAS']; ?>"><?php echo date("d-m-Y", strtotime($row['fecha'])); ?><?php echo ' - ' ?><?php echo date('H:s', strtotime($row['hora'])); ?></option>
+                                    <?php } ?>
+                                </select>
+                            </div>
                         </div>
-                        <div class="input-box">
-                            <span class="details" for="apellCLIENTE">Apellidos</span>
-                            <input type="text" placeholder="Ingrese su apellido" name="apellCLIENTE" required>
+                        <div class="container2" style="background-color:#f1f1f1">
+                            <button type="submit" value="agendar" class="agendarbtn">Agendar</button>
+                            <button type="button" onclick="document.getElementById('id02').style.display='none'" class="cancelbtn">Cancel</button>
                         </div>
-                        <div class="input-box">
-                            <span class="details" for="rutCLIENTE">Rut</span>
-                            <input type="text" placeholder="Ingrese su Rut" name="rutCLIENTE" oninput="checkRut(this)" required>
-                        </div>
-                        <div class="input-box">
-                            <span class="details" for="dirCLIENTE">Direccion</span>
-                            <input type="text" placeholder="Ingrese su Direccion" name="dirCLIENTE" required>
-                        </div>
-                        <div class="input-box">
-                            <span class="details" for="numCLIENTE">Numero Celular</span>
-                            <input class="code" type="text" placeholder="+56" readonly>
-                            <input type="tel" class="tel" name="numCLIENTE" placeholder="12345678" maxlength="8" required>
-                        </div>
-                        <div class="input-box">
-                            <span class="details" for="emailCLIENTE">Correo electronico</span>
-                            <input type="email" name="emailCLIENTE" placeholder="correo@gmail.com" required>
-                        </div>
-                        <div class="select-box">
-                            <span class="details">servicio:</span>
-                            <select name="cbx_servicios" id="cbx_servicios">
-                                <option value="0">seleccionar un servicio</option>
-                                <?php while ($row = $sql2->fetch_assoc()) {
-                                ?>
-                                    <option value="<?php echo $row['idSERVICIO']; ?>"><?php echo $row['nombre_servicio']; ?><?php echo ' - ' ?><?php echo '$' ?><?php echo $row['precio_servicio']; ?></option>
-                                <?php } ?>
-                            </select>
-                        </div>
-                        <div class="select-box">
-                            <span class="details">Seleccion Hora:</span>
-                            <select name="cbx_horas" id="cbx_horas">
-                                <option value="0">seleccionar Hora</option>
-                                <?php while ($row = $sql3->fetch_assoc()) {
-                                ?>
-                                    <option value="<?php echo $row['idHORAS']; ?>"><?php echo date("d-m-Y", strtotime($row['fecha'])); ?><?php echo ' - ' ?><?php echo date('H:s', strtotime($row['hora'])); ?></option>
-                                <?php } ?>
-                            </select>
-                        </div>
-                    </div>
-                    <div class="container2" style="background-color:#f1f1f1">
-                        <button type="submit" value="agendar" class="agendarbtn">Agendar</button>
-                        <button type="button" onclick="document.getElementById('id02').style.display='none'" class="cancelbtn">Cancel</button>
-                    </div>
-                </form>
+                    </form>
+                </div>
             </div>
-        </div>
 
 
 </body>
