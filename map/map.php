9b29fa820ecf467e177c8e7d27e42d1aff3abdb8
JuanjoEsp1 <you@example.com>
2022-11-07 01:34:06 +0000

    map

--- map/map.php
+++ map/map.php
@@ -8,7 +8,7 @@
 <div>
      <h3> Enter an adress and press the button</h3>
  
-    <input id="address" type="text" placeholder="Enter address here" />
+    <input id="direccion" type="text" placeholder="Enter address here" />
     <div>
         <p>Latitude:
             <input type="text" id="latitude" readonly />
@@ -23,16 +23,23 @@
 <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&key=AIzaSyAwPrp3BT2yJmxJQxIpIGNHn_p0hXxiTU8&libraries=places"></script>
  
 <script>
+var direccion = 'direccion';
 google.maps.event.addDomListener(window, 'load', initialize);
 function initialize() {
-var input = document.getElementById('address');
-var autocomplete = new google.maps.places.Autocomplete(input);
+
+var autocomplete;
+autocomplete = new google.maps.places.Autocomplete((document.getElementById(direccion)), {
+    componentRestrictions: {
+        country: "CL"
+    }
+});
 autocomplete.addListener('place_changed', function () {
 var place = autocomplete.getPlace();
 // place variable will have all the information you are looking for.
  
   document.getElementById("latitude").value = place.geometry['location'].lat();
   document.getElementById("longitude").value = place.geometry['location'].lng();
+
 });
 }
 </script>
