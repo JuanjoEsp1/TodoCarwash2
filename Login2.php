6eb4411c6df6d2476d0c4cb06b0dd150d3da782a
JuanjoEsp1 <108388391+JuanjoEsp1@users.noreply.github.com>
2022-11-15 23:32:13 -0300

    Update Login2.php

--- Login2.php
+++ Login2.php
@@ -41,10 +41,10 @@
 </body>
 
 <script>
-    // Obtener el modal
+    <!-- Obtener el modal -->
     var modal = document.getElementById('id01');
 
-    // Cuando el usuario haga clic en cualquier lugar fuera del modal, se cierra
+    <!-- Cuando el usuario haga clic en cualquier lugar fuera del modal, se cierra-->
     window.onclick = function(event) {
         if (event.target == modal) {
             modal.style.display = "none";
