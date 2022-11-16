a1dfcad64e0feda35b5d13b8e14b203c2a493820
JuanjoEsp1 <108388391+JuanjoEsp1@users.noreply.github.com>
2022-11-15 23:04:50 -0300

    Update IngresarServicios.php

--- IngresarServicios.php
+++ IngresarServicios.php
@@ -12,9 +12,9 @@
 <body>
 
     <?php
-
+    // Conexion Base de datos
     include("Funciones/db.php");
-
+    // Condicion para verificar si se recibieron los datos
     if (isset($_POST['guardar_servicio'])) {
         $servicio = $_POST['servicio'];
         $precio = $_POST['precio'];
@@ -24,8 +24,9 @@
             $s_servicio = $servicios;
             $s_precio = $precio[$index];
             $s_descripcion = $descripcion[$index];
-
+            // Consulta para insertar los datos del servicio a la base de datos
             $query = "INSERT INTO servicio (nombre_servicio,precio_servicio,descripcion,EMPRESA_idEmpresa) VALUES ('$s_servicio','$s_precio','$s_descripcion','$_REQUEST[idEmpresa]')";
+            // Ejecutar la consulta
             $query_run = mysqli_query($conexion, $query);
         }
 
